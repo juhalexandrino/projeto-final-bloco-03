9253849b23ba04c12453ab650449ebe1fd9f6f5b
juhalexandrino <juliasalexandrino@hotmail.com>
2023-11-09 10:16:08 -0300

    primeira entrega

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+function Footer() {
+
+    let data = new Date().getFullYear()
+
+    return (
+        <>
+            <div className="flex justify-center bg-indigo-900 text-white">
+                <div className="container flex flex-col items-center py-4">
+                    <p className='text-xl font-bold'>
+                            Projeto final bloco 03: Farmácia | Copyright: {data}
+                        </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer
